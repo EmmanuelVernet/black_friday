eeaabd4a17620c7edd622d4caaa500b6534f6b5f
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-12-09 17:43:14 +0100

    bundle install active support + basic methods...

--- lib/black_friday.rb
+++ lib/black_friday.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
 require_relative "black_friday/version"
+require "activesupport/all"
 
 module BlackFriday
   class Error < StandardError; end
   # Your code goes here...
+  def thanksgiving
+    first_thursday + 3.weeks
+  end
+  
+  def black_friday
+    thanksgiving + 1.day
+  end
+  
+  def cyber_monday
+    thanksgiving + 4.days
+  end
+
+  def nov_first
+
+  end
 end
