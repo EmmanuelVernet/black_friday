a84d3bb323abc7fb4615145c4cf67917a438945f
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-12-09 17:50:09 +0100

    added nov first and thurdsday methods

--- lib/black_friday.rb
+++ lib/black_friday.rb
@@ -6,19 +6,25 @@ require "activesupport/all"
 module BlackFriday
   class Error < StandardError; end
   # Your code goes here...
-  def thanksgiving
-    first_thursday + 3.weeks
-  end
-  
-  def black_friday
-    thanksgiving + 1.day
-  end
-  
-  def cyber_monday
-    thanksgiving + 4.days
-  end
+  class << self
+    def thanksgiving
+      first_thursday + 3.weeks
+    end
+    
+    def black_friday
+      thanksgiving + 1.day
+    end
+    
+    def cyber_monday
+      thanksgiving + 4.days
+    end
 
-  def nov_first
+    def nov_first
+      Date.new(Date.today.year, 11, 1)
+    end
 
+    def first_thursday
+      nov_first.thursday? ? nov_first : nov_first.next_occurring(:thursday)
+    end
   end
 end
